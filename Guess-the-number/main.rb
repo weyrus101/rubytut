ed99951fd6dd07a16856218d3f9f07f211f71b91
weyrus101 <weyrus101@gmail.com>
2019-11-18 18:20:44 +0300

    Mod-1.0

---
+++ Guess-the-number/main.rb
@@ -0,0 +1,38 @@
+if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+puts("Привет! Меня зовут Джек. Как я могу называть тебя?")
+user = gets.chomp # запрос имени
+puts("Хорошо, #{user}. Давай сыграем в игру \"Угадай-ка!\".\nЯ загадываю число от 1 до 16, у тебя 3 попытки, чтобы его отгадать.")
+number = rand(1..16) # рандомное число (загаданное)
+attempts = 4 # кол-во попыток
+answer = nil # ответ пользователя
+
+def answer_false(answer, number)
+	if answer > number
+		(answer - number).abs <= 2 ? 
+			puts(">> Тепло, но нужно меньше!") : 
+			puts(">> Холодно, нужно меньше!")
+	elsif answer < number
+		(number - answer).abs <= 2 ? 
+			puts(">> Тепло, но нужно больше!") : 
+			puts(">> Холодно, нужно больше!")
+	end
+end
+
+while (number != answer) & (attempts != 0)
+	attempts-=1
+	answer = gets.to_i
+
+	answer == number ? 
+		puts("Совершенно верно! Мой число - #{number}. Ты отгадал его за #{3 - attempts} попыток!") : 
+		answer_false(answer, number)
+end
+
+((attempts == 0) & (number != answer)) ? puts("Увы, ты проиграл. Моё число - #{number}.") : nil
