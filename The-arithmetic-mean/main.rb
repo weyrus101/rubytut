dad04e2527a4f9982b0c63f34420a1613619ad26
weyrus101 <weyrus101@gmail.com>
2019-11-18 18:08:06 +0300

    Mod-1.1

--- The-arithmetic-mean/main.rb
+++ The-arithmetic-mean/main.rb
@@ -3,4 +3,12 @@ numbers = STDIN.gets
 
 numbers_a = numbers.split(" ") # Преобразовываем в массив
 divider = numbers_a.size # Кол-во элементов (делитель)
+result = 0
 
+(0..(divider-1)).each do |i|
+	result = result + numbers_a[i].to_f
+end
+
+result = result / divider
+
+puts "Среднее арифметическое: #{result}"
